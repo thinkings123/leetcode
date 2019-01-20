56b9cd14275647d7195b42d3d875c15ab80cccf5
yyr <1241114834@qq.com>
2019-01-20 23:43:44 +0800

    finish Two_Sum

--- Two Sum.py
+++ Two Sum.py
@@ -1,1 +1,12 @@
-print("This is my 2019's first code")
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic = {} # key is num, value is index
+        for i, num in enumerate(nums):
+            if target - num in dic:
+                return [dic[target-num], i]
+            dic[num] = i
---
+++ readme.md
@@ -0,0 +1,4 @@
+## Leetcode
+
+This is my leetcode
+
